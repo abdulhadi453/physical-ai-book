ee36c9a864e69d164a476b65c075c0d05de6b07e
abdulhadi453 <ah6335171@gmail.com>
2025-12-20 12:30:48 +0500

    third commit

---
+++ physical-ai-book/.docusaurus/client-modules.js
@@ -0,0 +1,7 @@
+export default [
+  require("C:\\Users\\ICTech\\Desktop\\hackathon-book\\physical-ai-book\\.docusaurus\\docusaurus-plugin-css-cascade-layers\\default\\layers.css"),
+  require("C:\\Users\\ICTech\\Desktop\\hackathon-book\\physical-ai-book\\node_modules\\infima\\dist\\css\\default\\default.css"),
+  require("C:\\Users\\ICTech\\Desktop\\hackathon-book\\physical-ai-book\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
+  require("C:\\Users\\ICTech\\Desktop\\hackathon-book\\physical-ai-book\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
+  require("C:\\Users\\ICTech\\Desktop\\hackathon-book\\physical-ai-book\\src\\css\\custom.css"),
+];
